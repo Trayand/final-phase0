26afddd22ea0905a02273b803994fef70bad7b1a
Trayand <patradyn@gmail.com>
2019-10-25 19:46:35 +0700

    Timer Done

--- timer2.js
+++ timer2.js
@@ -102,3 +102,15 @@ function reset(){
 		lenyap[i].value = 'blank'
 	}
 }
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
